ce98b982ee88ae42dd49920cff46e9fe5edb259a
Ivan Hdz <ivan.daniel.hdz@gmail.com>
2022-05-16 17:07:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,113 +1,116 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useState, useEffect } from 'react';
-import Usuarios from './services/Usuarios';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
 
-  var values = [10, 1000, 50, 0, 100]
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
-  const [user, setUser] = useState(1);
-  const [result, setResult] = useState(0);
-  const [userLog, setUserLog] = useState(null);
-  const [stock, setStock] = useState(400);
-  const usuario = {
-    UName: 'ivan2',
-    pass: 'pass2'
-  }
-
-  useEffect(() => {
-    if (stock < 100) {
-      alert('Low Stock');
-    }
-  }, [stock])
-
-  function login() {
-    Usuarios.create(usuario).then(response => {
-      if (response.status === 200) {
-        console.log(response.data);
-      }
-    })
-  }
-
-  function changeUser() {
-    setUser(user + 1);
-  }
-
-  function applyBtn(value) {
-    setResult((value * 2) / 5);
-    setStock(stock - 20);
-    setUserLog(user);
-  }
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Example
-        </h1>
-        <h2>User {user}</h2>
-        <h3>Stock {stock}</h3>
-        <button onClick={login} >Login</button>
-        <table>
-          <tbody>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Action</th>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Aux1</td>
-              <td>{values[0]}</td>
-              <td><button onClick={() => applyBtn(values[0])}>Apply</button></td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Aux2</td>
-              <td>{values[1]}</td>
-              <td><button onClick={() => applyBtn(values[1])}>Apply</button></td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Aux3</td>
-              <td>{values[2]}</td>
-              <td><button onClick={() => applyBtn(values[2])}>Apply</button></td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>Aux4</td>
-              <td>{values[3]}</td>
-              <td><button onClick={() => applyBtn(values[3])}>Apply</button></td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>Aux5</td>
-              <td>{values[4]}</td>
-              <td><button onClick={() => applyBtn(values[4])}>Apply</button></td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <tbody>
-            <tr>
-              <th>Result</th>
-              <th>User</th>
-            </tr>
-            <tr>
-              <td>{result}</td>
-              <td>{userLog}</td>
-            </tr>
-          </tbody>
-        </table>
-      </header>
-      <div>
-        <button onClick={changeUser}>Change User</button>
-      </div>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+}

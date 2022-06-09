91ea27bd3122cfd661cf63bdb4a969059af724d0
Ivan Hdz <ivan.daniel.hdz@gmail.com>
2022-06-09 15:48:22 -0500

    UtopiaInventory prod

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://localhost:7213/api",
-    // baseURL: "http://salon-api-dev.us-east-2.elasticbeanstalk.com/api",
+    // baseURL: "https://localhost:7213/api",
+    baseURL: "http://utopia-api.us-east-2.elasticbeanstalk.com/api",
     headers: {
         "Content-type": "application/json"
     }

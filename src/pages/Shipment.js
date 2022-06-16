0e67b69f4ddafd62eb3e87395ee6764c3938b2ed
Ivan Hdz <ivan.daniel.hdz@gmail.com>
2022-06-16 16:36:40 -0500

    Shipment beta

--- src/pages/Shipment.js
+++ src/pages/Shipment.js
@@ -20,15 +20,14 @@ import ShipmentService from '../services/ShipmentService';
 
 export default function Shipment() {
     const [rows, setRows] = useState([]);
-    const [open, setOpen] = React.useState(false);
+    const [openDoA, setOpenDoA] = useState(false);
+    const [checked, setChecked] = useState(false);
+    const [dateArrival, setDateArrival] = useState('');
+    const [disabled, setDisabled] = useState(false);
+    const [openShipment, setOpenShipment] = useState(false);
+    const [dateShipment, setDateShipment] = useState('');
+    const [serial, setSerial] = useState(0);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
 
     useEffect((rows) => {
@@ -40,6 +39,46 @@ export default function Shipment() {
             }
         });
     }, [])
+
+    const handleClickOpenDoA = () => {
+        setOpenDoA(true);
+        setChecked(true);
+    };
+
+    const handleAcceptDoA = () => {
+        setOpenDoA(false);
+        setChecked(true);
+        setDisabled(true);
+    }
+
+    const handleCloseDoA = () => {
+        setOpenDoA(false);
+        setChecked(false);
+    };
+
+    const handleDoAChange = (event) => {
+        setDateArrival(event.target.value);
+    };
+
+    const handleOpenShipment = () => {
+        setOpenShipment(true);
+    }
+
+    const handleCloseShipment = () => {
+        setOpenShipment(false);
+    }
+
+    const handleDateShipmentChange = (event) => {
+        setDateShipment(event.target.value);
+    };
+
+    const handleSerialChange = (event) => {
+        setSerial(event.target.value);
+    };
+
+    const handleAcceptShipment = () => {
+        setOpenShipment(false);
+    };
 
     return (
         <>
@@ -58,58 +97,92 @@ export default function Shipment() {
                     </TableHead>
                     <TableBody>
                         {rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">{row.id}</TableCell>
-                                <TableCell>{row.sNumber}</TableCell>
-                                <TableCell>{row.details}</TableCell>
-                                {row.shipped === 1 ? <Checkbox disabled defaultChecked color="default" /> : <Checkbox />}
-                                <TableCell>{row.shipDate}</TableCell>
-                                {row.received === 1 ? <Checkbox defaultChecked color="default" /> : <Checkbox />}
-                                <TableCell>{row.arriveDate}</TableCell>
-                            </TableRow>
+                            row.received === 1 ?
+                                <TableRow
+                                    key={row.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">{row.id}</TableCell>
+                                    <TableCell>{row.sNumber}</TableCell>
+                                    <TableCell>{row.details}</TableCell>
+                                    <Checkbox disabled defaultChecked color="default" />
+                                    <TableCell>{row.shipDate}</TableCell>
+                                    <Checkbox disabled defaultChecked color="default" />
+                                    <TableCell>{row.arriveDate}</TableCell>
+                                </TableRow> :
+                                <TableRow
+                                    key={row.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">{row.id}</TableCell>
+                                    <TableCell>{row.sNumber}</TableCell>
+                                    <TableCell>{row.details}</TableCell>
+                                    <Checkbox disabled defaultChecked color="default" />
+                                    <TableCell>{row.shipDate}</TableCell>
+                                    <Checkbox disabled={disabled} checked={checked} onChange={handleClickOpenDoA} color="default" />
+                                    <TableCell>{row.arriveDate}</TableCell>
+                                </TableRow>
                         ))}
 
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Button variant="text" onClick={handleClickOpen}>Add shipment</Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Authorization needed</DialogTitle>
+            <Button variant="text" onClick={handleOpenShipment} >Add shipment</Button>
+            <Dialog open={openDoA} onClose={handleCloseDoA}>
+                <DialogTitle>Date of arrival</DialogTitle>
                 <DialogContent>
                     {/* {() => handleAlert(success, error)} */}
                     <DialogContentText>
-                        Introduce User and Password for authorization
+                        Introduce shipment's date of arrival
                     </DialogContentText>
                     <div>
                         <TextField
                             autoFocus
                             margin="dense"
-                            id="user"
-                            label="User"
-                            type="email"
+                            id="date"
+                            type="Date"
                             variant="standard"
-
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="password"
-                            label="Password"
-                            type="pasword"
-                            variant="standard"
-
+                            onChange={handleDoAChange}
                         />
                     </div>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleCloseDoA}>Cancel</Button>
                     <Button onClick={() => {
-                        handleClose()
+                        handleAcceptDoA()
+                    }}>Accept</Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={openShipment} onClose={handleCloseShipment}>
+                <DialogTitle>New Shipment</DialogTitle>
+                <DialogContent>
+                    {/* {() => handleAlert(success, error)} */}
+                    <DialogContentText>
+                        Introduce shipment's data
+                    </DialogContentText>
+                    <div>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="serial"
+                            label="Serial Number"
+                            variant="standard"
+                            onChange={handleDateShipmentChange}
+                        />
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="date"
+                            type="Date"
+                            variant="standard"
+                            onChange={handleSerialChange}
+                        />
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseShipment}>Cancel</Button>
+                    <Button onClick={() => {
+                        handleAcceptShipment()
                     }}>Accept</Button>
                 </DialogActions>
             </Dialog>
